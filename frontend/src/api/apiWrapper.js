85de0f3477ecf7cb9a3b39b7178488431b1db5e7
vostsoldier <fantasyindeath86@gmail.com>
2025-02-09 13:17:32 -0500

    maybe fix

--- frontend/src/api/apiWrapper.js
+++ frontend/src/api/apiWrapper.js
@@ -1,4 +1,21 @@
-import axiosInstance from './axios';
+import axios from 'axios';
+
+const token = localStorage.getItem('token');
+
+const api = axios.create({
+  baseURL: 'https://think-tree-production.up.railway.app/api',
+  headers: {
+    'Content-Type': 'application/json',
+    ...(token && { Authorization: `Bearer ${token}` })
+  }
+});
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
 
 const apiWrapper = {
   get: (url, config = {}) => {
@@ -6,31 +23,31 @@ const apiWrapper = {
       console.log(`Demo mode: GET request to ${url} blocked.`);
       return Promise.reject(new Error('Demo mode: GET requests are disabled.'));
     }
-    return axiosInstance.get(url, config);
+    return api.get(url, config);
   },
   post: (url, data, config = {}) => {
     if (url === '/api/users/signup' || url === '/api/users/login') {
-      return axiosInstance.post(url, data, config);
+      return api.post(url, data, config);
     }
     if (localStorage.getItem('isDemo') === 'true' && !localStorage.getItem('token')) {
       console.log(`Demo mode: POST request to ${url} blocked.`);
       return Promise.reject(new Error('Demo mode: POST requests are disabled.'));
     }
-    return axiosInstance.post(url, data, config);
+    return api.post(url, data, config);
   },
   put: (url, data, config = {}) => {
     if (localStorage.getItem('isDemo') === 'true') {
       console.log(`Demo mode: PUT request to ${url} blocked.`);
       return Promise.reject(new Error('Demo mode: PUT requests are disabled.'));
     }
-    return axiosInstance.put(url, data, config);
+    return api.put(url, data, config);
   },
   delete: (url, config = {}) => {
     if (localStorage.getItem('isDemo') === 'true') {
       console.log(`Demo mode: DELETE request to ${url} blocked.`);
       return Promise.reject(new Error('Demo mode: DELETE requests are disabled.'));
     }
-    return axiosInstance.delete(url, config);
+    return api.delete(url, config);
   },
 };
 
